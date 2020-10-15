8fb30edbf3922963882fe1d20ca0417b52d8f455
naveen joshua <naveenjoshua@gmail.com>
2020-10-16 01:01:05 +0530

    again added

--- src/test/java/com/un/wm/caos/CaosApplicationTests_Renamed.java
+++ src/test/java/com/un/wm/caos/CaosApplicationTests_Renamed.java
@@ -12,14 +12,8 @@ class CaosApplicationTests {
 
 
 	/**
-		This is a sample text commit.
+		This is a sample text commit. Again added
 	 */
 
-	 /**
-		This is a sample text commit2.
-	 */
-	/**
-		can we ammend this change along with that
-	 */
 
 }
